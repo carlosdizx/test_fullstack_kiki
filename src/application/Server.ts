308525fafc0ee8841d1fcddee03e3a0bf1cd768b
Carlos Ernesto Diaz <carlodiaz@umariana.edu.co}>
2023-01-18 18:06:43 -0500

    Installed types of node and config run app in the port dynamic

--- src/application/Server.ts
+++ src/application/Server.ts
@@ -9,4 +9,14 @@ export class  Server{
         this.port = port;
         this.app = express();
     }
+
+    listen = async (): Promise<void> => {
+        return new Promise((resolve) => {
+            this.app.listen(this.port, () => {
+                console.log(`App is running at http://localhost:${this.port} in ${this.app.get("env")} mode`);
+                console.log("Press CTRL-C to stop\n");
+                resolve();
+            });
+        });
+    };
 }
