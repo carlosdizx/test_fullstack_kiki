8e7190adb6b8b4245931ab646656927dbdb7e66e
Carlos Ernesto Diaz <carlodiaz@umariana.edu.co}>
2023-01-18 18:48:03 -0500

    Defined environment variables for PostgreSQL and created postgresSQL configuration file

---
+++ src/application/PostgreSQLConfig.ts
@@ -0,0 +1,23 @@
+import { DataSource } from "typeorm";
+
+export const PostgresSQLDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_POSTGRES_HOST,
+  port: parseInt(`${process.env.DB_POSTGRES_PORT}`),
+  username: process.env.DB_POSTGRES_USER,
+  password: process.env.DB_POSTGRES_PASSWORD,
+  database: process.env.DB_POSTGRES_DATABASE,
+  entities: [],
+});
+
+const dbPostgresSQLInit = async () => {
+  try {
+    console.log("Connecting to PostgreSQL");
+    await PostgresSQLDataSource.initialize();
+    console.log("Connected to PostgreSQL");
+  } catch (e) {
+    console.error("Error during Data Source initialization", e);
+  }
+};
+
+export default dbPostgresSQLInit;
