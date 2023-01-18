d1640e3e5393fae67858bdc6a443edfe626706c3
Carlos Ernesto Diaz <carlodiaz@umariana.edu.co}>
2023-01-18 18:34:05 -0500

    installed typeorm for map entities to database

---
+++ src/infrastructure/repository/common/CrudOperations.ts
@@ -0,0 +1,10 @@
+import { DataSource } from "typeorm";
+
+export interface CrudOperations<ID> {
+  save(entity: any): Promise<any>;
+  list(): Promise<any>;
+  findById(id: ID): Promise<any>;
+  update(entity: any, id: ID): Promise<any>;
+  delete(id: ID): Promise<void>;
+  dataSource(): DataSource;
+}
