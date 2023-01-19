07b777737108337d8b5258af94c84b284d9c9b22
Carlos Ernesto Diaz <carlodiaz@umariana.edu.co}>
2023-01-18 19:06:44 -0500

    installed pg for connect to postgreSQL, configured file for create database initial and called in the docker-compose.yml

--- src/application/Server.ts
+++ src/application/Server.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express, {Express} from "express";
+import dbPostgresSQLInit from "./PostgreSQLConfig";
 
 export class  Server{
     private readonly port: string;
@@ -8,7 +9,12 @@ export class  Server{
     constructor(port: string) {
         this.port = port;
         this.app = express();
+        this.connectToDatabase().then();
     }
+
+    connectToDatabase = async () => {
+        await dbPostgresSQLInit();
+    };
 
     listen = async (): Promise<void> => {
         return new Promise((resolve) => {
