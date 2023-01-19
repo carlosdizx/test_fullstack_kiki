f0ab31943dc10c52db68ea87eb09309709fc64f2
Carlos Ernesto Diaz <carlodiaz@umariana.edu.co}>
2023-01-19 09:37:21 -0500

    change port and config docker-compose.yml and docker_postgres_init.sql

--- docker_postgres_init.sql
+++ docker_postgres_init.sql
@@ -2,6 +2,6 @@ CREATE DATABASE prueba;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE civilized_pokemons(    uuid UUID DEFAULT uuid_generate_v4(),    name VARCHAR,    document_number VARCHAR,    document_type VARCHAR,    species VARCHAR,   type VARCHAR);
+CREATE TABLE IF NOT EXISTS civilized_pokemons(uuid UUID DEFAULT uuid_generate_v4(),name VARCHAR,document_number VARCHAR,document_type VARCHAR,species VARCHAR, type VARCHAR);
 
 
